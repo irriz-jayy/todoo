d8dba0a740fac1eedd0166d82f70d4dc9a852d22
irriz-jayy <jayfelix789@gmail.com>
2023-07-19 11:22:10 +0300

    created adding of todos

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TodoForm() {
+  const [todoValue, setTodoValue] = useState("");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(todoValue);
+
+    setTodoValue("");
+  };
   return (
     <>
-      <input type="text" value="" placeholder="Add tasks" />
-      <button>Create task</button>
+      <input
+        type="text"
+        value={todoValue}
+        placeholder="Add tasks"
+        onChange={(e) => setTodoValue(e.target.value)}
+      />
+      <button onClick={handleClick}>Create task</button>
     </>
   );
 }
